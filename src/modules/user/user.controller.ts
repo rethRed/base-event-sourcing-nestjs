4a39b310ddaadf0173333422f02abac4627210ef
rethred <gabrielwastha@gmail.com>
2023-07-12 05:25:41 -0300

    change user controller and service

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -12,23 +12,4 @@ export class UserController {
     return this.userService.create(createUserDto);
   }
 
-  @Get()
-  findAll() {
-    return this.userService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.userService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.userService.update(+id, updateUserDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.userService.remove(+id);
-  }
 }
--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -45,19 +45,4 @@ export class UserService {
 
   }
 
-  findAll() {
-    return `This action returns all user`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
-  }
-
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} user`;
-  }
 }
