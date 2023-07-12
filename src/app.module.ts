6eebaedfe52e7230d2ae762e18be019e8e51a640
rethred <gabrielwastha@gmail.com>
2023-07-12 11:27:31 -0300

    change auth module name

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
-import { AuthModule } from './modules/user/user.module';
+import { AuthModule } from './modules/user/auth.module';
 import { RabbitmqOutboxModule } from './infra/rabbitmq-outbox/rabbitmq-outbox.module';
 import { RabbitModule } from './rabbitmq';
 import { EventSourcingModule } from './infra/event-sourcing/event-sourcing.module';
